d78690fd0e42b68b4aa96764a84393af41f5d1e6
Axel Latvala <axel@latvala.com>
2018-11-20 15:05:45 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>BitSaber DevOps Platform</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="screen" />
+        <link rel="shortcut icon" href="/images/favicon.png" />
+    </head>
+    <body>
+        <div id="content">
+            <div class="header codefont">
+                BitSaber
+            </div>
+            <div id="appCatalog">
+                <div class="app" id="jira">
+                    <a href="https://jira.bitsaber.net">
+                        <div class="overlay">
+                            <div class="apptitle">Jira</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="app" id="confluence">
+                    <a href="https://confluence.bitsaber.net">
+                        <div class="overlay">
+                            <div class="apptitle">Confluence</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="app" id="jenkins">
+                    <a href="https://jenkins.bitsaber.net">
+                        <div class="overlay">
+                            <div class="apptitle">Jenkins</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="app" id="sonar">
+                    <a href="https://sonar.bitsaber.net">
+                        <div class="overlay">
+                            <div class="apptitle">SonarQube</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="app" id="github">
+                    <a href="https://github.com/BitSaber">
+                        <div class="overlay">
+                            <div class="apptitle">GitHub</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="app" id="artifactory">
+                    <a href="https://artifactory.bitsaber.net">
+                        <div class="overlay">
+                            <div class="apptitle">Artifactory</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
